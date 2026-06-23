95b64bfbed9a80e164b3b2455947cbb04450c72c
federicoorsini73 <federicoorsini73@gmail.com>
2026-06-23 16:39:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = 'zodiaco-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'zodiaco-v2';
 
+// Installazione del Service Worker
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+// Attivazione e pulizia vecchia cache
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
+// Gestione delle richieste (passa tutto direttamente alla rete)
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
